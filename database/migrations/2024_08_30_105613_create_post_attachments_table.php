a2d37ab7d61df8f12ec00244259e20411d41b951
Dev Nome <63138189+NumanKhan0103@users.noreply.github.com>
2024-08-30 19:27:43 +0500

    ready migration and migrate in database

--- database/migrations/2024_08_30_105613_create_post_attachments_table.php
+++ database/migrations/2024_08_30_105613_create_post_attachments_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('post_attachments', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('post_id')->constrained('posts');
+            $table->string('name', 255); // test.png
+            $table->string('path', 255); // where file is located
+            $table->string('mime', 25);  // image/png, video etc 
+            $table->foreignId('created_by')->constrained('users');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
