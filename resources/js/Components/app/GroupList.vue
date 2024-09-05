05be58683e23b8a69ac7a381413da040e7914a5e
Dev Nome <63138189+NumanKhan0103@users.noreply.github.com>
2024-09-05 19:36:12 +0500

    make separate group list and group item component for group section

---
+++ resources/js/Components/app/GroupList.vue
@@ -0,0 +1,32 @@
+<script setup >
+import GroupItem from '../Components/app/GroupItem.vue';
+import TextInput from '../Components/TextInput.vue';
+
+const searchKeyword = ref('');
+
+</script>
+
+<template>
+ <h2 class="text-2xl font-bold px-3"> My Group</h2>
+ <TextInput :model-value="searchKeyword" />
+
+            <div class="py-8">
+                <div v-if="false" class="text-gray-400 flex text-center">
+                    You are not joined to any groups
+                </div>
+                <div v-else>
+                    
+                    <GroupItem  
+                    image="https://picsum.photos/100" 
+                    title="Laravel Developer"
+                    description="Lorem ipsum dolor sit amet, consectetur."
+                    />
+                    <GroupItem  
+                    image="https://picsum.photos/100" 
+                    title="Vue js Developer"
+                    description="Lorem ipsum dolor sit amet, consectetur."
+                    />
+
+                </div>
+            </div>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,57 +1,31 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
+// import GroupList from "../components/app/GroupList.vue";
 
-defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
-});
+defineProps({});
 
-function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
-}
 </script>
 
 <template>
+
     <Head title="Social Media App" />
-   
+
     <div class="grid grid-cols-12 gap-3">
-    <div class="col-span-3">
-        <h2> My Group</h2>
-        <div>
-            You are not joined to any groups
+        <div class="col-span-3  py-6 px-3">
+
+            <GroupList />
+
         </div>
-        <div>
-            <div class="flex">
-                <img src="https://picsum.photos/100" alt="">
-                <div>
-                    <h3>Laravel Developer</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur.</p>
-                </div>
-            </div>
+
+
+        <div class="col-span-6">
+            Posts
+        </div>
+
+
+        <div class="col-span-3">
+            Followers
         </div>
     </div>
-    <div class="col-span-6">
-        Posts
-    </div>
-    <div class="col-span-3">
-        Followers
-    </div>
-</div>
 
 
 </template>
